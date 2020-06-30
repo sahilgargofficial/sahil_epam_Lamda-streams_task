c177a82d1d8883f7aae2e003a95cab4e09d53c7f
sahilgargofficial <sahilgargofficial@gmail.com>
2020-06-30 11:32:54 +0530

    epam task

---
+++ src/main/java/org/example/palindromeStrings.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class palindromeStrings {
+    public static void checkPalindrome(List<String> l1) {
+        List<String> l2 = new ArrayList<String>();
+        for (String i : l1) {
+            String t = i.replaceAll("\\s+", "").toLowerCase();
+            if (IntStream.range(0, t.length() / 2).noneMatch(j -> t.charAt(j) != t.charAt(t.length() - j - 1)))
+                l2.add(i);
+        }
+        System.out.println("Palindrome Strings are: " + l2);
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("apples");
+        list.add("aeiiea");
+        list.add("banana");
+        list.add("tooioot");
+        System.out.println("List is: " + list);
+        palindromeStrings.checkPalindrome(list);
+    }
+}
